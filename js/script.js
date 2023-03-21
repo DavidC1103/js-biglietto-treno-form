8d14623a6a48ba02fb293b50e5e71db9ebda2f50
David Calaiò <davidcjr11@gmail.com>
2023-03-21 15:02:11 +0100

    add some const and button, input on html

--- js/script.js
+++ js/script.js
@@ -4,3 +4,32 @@ const kmPrice = 0.21;
 const disc20 = 20;
 //sconto >65
 const disc40 = 40;
+
+const nameUser = document.getElementById('name')
+console.log('name', name);
+
+const kmUser = document.getElementById('km')
+console.log('km', km);
+
+const button1 = document.getElementById('button1')
+
+const button2 = document.getElementById('button2')
+
+
+button1.addEventListener('click', function(){
+    
+    km = kmUser.value; 
+    console.log('km da percorrere', km);
+
+    let standardPrice =  kmPrice * km;
+    console.log('prezzo standard', standardPrice);
+    
+    
+})
+
+
+
+
+
+
+
